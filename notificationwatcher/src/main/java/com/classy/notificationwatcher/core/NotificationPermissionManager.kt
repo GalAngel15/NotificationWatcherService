d69c25b64d8bc7ee3838b7105c092903d07b698b
GalAngel15 <galangel151@gmail.com>
2025-08-05 05:46:20 +0300

    fixed

--- notificationwatcher/src/main/java/com/classy/notificationwatcher/core/NotificationPermissionManager.kt
+++ notificationwatcher/src/main/java/com/classy/notificationwatcher/core/NotificationPermissionManager.kt
@@ -5,7 +5,6 @@ import android.app.AlertDialog
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
-import android.os.Build
 import android.os.PowerManager
 import android.provider.Settings
 import androidx.core.app.NotificationManagerCompat
